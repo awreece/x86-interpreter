fd2de14c5b96a8314b8c25f60738803e1fd5eef5
Alex Reece <awreece@gmail.com>
2013-08-16 01:41:13 -0700

    refactor to app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+function StateController($scope) {
+  $scope.all_registers = all_registers;
+  $scope.state = new State();
+  $scope.range = range;
+  $scope.hex = hex;
+}
+
+var mod = angular.module('x86interpreter', ['ui.bootstrap']);
+mod.factory('$exceptionHandler', function () {
+  return function (exception, cause) {
+    alert(exception);
+  };
+});
+mod.directive('focusOn', function() {
+  return function(scope, element, attrs) {
+    scope.$watch(attrs.focusOn, function (condition) {
+      if (condition) {
+        element[0].focus();
+      }
+    });
+  }
+});
+mod.directive('integer', function(){
+  return {
+    require: 'ngModel',
+    link: function(scope, ele, attr, ctrl){
+      ctrl.$parsers.unshift(function(viewValue) {
+        if (viewValue == "0x" || viewValue == "0X") {
+          return 0;
+        }
+        return parseInt(viewValue);
+      });
+      ctrl.$formatters.push(hex);
+    }
+  };
+});
+mod.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
